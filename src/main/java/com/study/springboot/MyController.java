6040ae86daf05207f69929967a6bdcbb3c4b3b39
DESKTOP-25TV5SJ\BigData <gikimirane@naver.com>
2024-08-30 15:28:02 +0900

    프로젝트 기초 완성

--- src/main/java/com/study/springboot/MyController.java
+++ src/main/java/com/study/springboot/MyController.java
@@ -16,24 +16,4 @@ public class MyController {
     public String test1() {
         return "test1";          // 실제 호출 될 /WEB-INF/views/test1.jsp       
     }
-
-    @RequestMapping("/test2")
-    public @ResponseBody String test2() {
-        return "test2";       
-    }
-
-    @RequestMapping("/test3")
-    public String test3() {
-        return "test3";       
-    }
-
-    @RequestMapping("/test4")
-    public String test4() {
-        return "test4";       
-    }
-
-    @RequestMapping("/test5")
-    public @ResponseBody String test5() {
-        return "test5";       
-    }
 }
